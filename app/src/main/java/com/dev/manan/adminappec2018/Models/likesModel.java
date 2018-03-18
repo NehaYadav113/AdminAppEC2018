5cdfa91423a1ddd8a59eb519e9c0740414301de0
Shubham Goel <goyalshubham73@gmail.com>
2018-03-18 14:46:41 +0530

    resolve conflicts for post

--- app/src/main/java/com/dev/manan/adminappec2018/Models/likesModel.java
+++ app/src/main/java/com/dev/manan/adminappec2018/Models/likesModel.java
@@ -4,23 +4,15 @@ package com.dev.manan.adminappec2018.Models;
  * Created by KASHISH on 11-03-2018.
  */
 
+
 public class likesModel {
     public String fid;
+    public likesModel(){
 
-    public likesModel(){}
-
+    }
     public likesModel(String fid) {
         this.fid=fid;
     }
-
-    public String getFid() {
-        return fid;
-    }
-
-    public void setFid(String fid) {
-        this.fid = fid;
-    }
-
     public void setfid(String comment){
         this.fid=comment;
     }
