77b52a0bbe333fc84e6d081c4be9c34a39ffd7d4
Himanshu Vishwakarma <h.vishwakarma20@gmail.com>
2018-03-23 22:26:04 +0530

    Uploading Post Image

--- app/src/main/java/com/dev/manan/adminappec2018/BrixxActivity.java
+++ app/src/main/java/com/dev/manan/adminappec2018/BrixxActivity.java
@@ -4,9 +4,13 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -17,11 +21,21 @@ import android.widget.Toast;
 import com.dev.manan.adminappec2018.Models.NotificationModel;
 import com.dev.manan.adminappec2018.Models.PostModel;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Random;
 
 
 public class BrixxActivity extends AppCompatActivity {
@@ -36,6 +50,10 @@ public class BrixxActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private String clubName = "None";
     private String CLUB_NAME = "clubname";
+    private Uri filePath = null;
+    private final int PICK_IMAGE_REQUEST = 71;
+    private StorageReference mStorageRef;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +61,8 @@ public class BrixxActivity extends AppCompatActivity {
         setContentView(R.layout.activity_brixx);
 
         com.google.firebase.messaging.FirebaseMessaging.getInstance().subscribeToTopic("Topic");
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
         prefs = getApplicationContext().getSharedPreferences("com.dev.manan.adminappec2018", Context.MODE_PRIVATE);
         final String token = prefs.getString("token", "");
@@ -142,6 +162,7 @@ public class BrixxActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(BrixxActivity.this, "Attach Poster Code!", Toast.LENGTH_SHORT).show();
+                chooseImage();
             }
         });
         postButton.setOnClickListener(new View.OnClickListener() {
@@ -149,29 +170,86 @@ public class BrixxActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Boolean checker = validateCredentialsPost();
                 pd.show();
-                String textPostTitle = editTextPostTitle.getText().toString();
+                final String textPostTitle = editTextPostTitle.getText().toString();
 
                 if (checker) {
-                    long time = System.currentTimeMillis();
-                    String postID = mDatabase.push().getKey();
-                    String urlPhotoTemp = "https://firebasestorage.googleapis.com/v0/b/culmyca2018.appspot.com/o/final_image.jpeg?alt=media&token=e7685e47-6762-42d9-bc12-8b8484e0fe38";
+                    final long time = System.currentTimeMillis();
+                    final String postID = mDatabase.push().getKey();
+                    final String[] urlPhotoTemp = {"https://firebasestorage.googleapis.com/v0/b/culmyca2018.appspot.com/o/final_image.jpeg?alt=media&token=e7685e47-6762-42d9-bc12-8b8484e0fe38"};
 
-                    PostModel pm = new PostModel(textPostTitle, urlPhotoTemp, clubName, time);
+                    StorageReference mountainsRef = mStorageRef.child("postImages/" + token + getSaltString() + ".jpg");
+                    if (filePath != null) {
+                        final ProgressDialog progressDialog = new ProgressDialog(BrixxActivity.this);
+                        progressDialog.setTitle("Uploading...");
+                        progressDialog.setCancelable(false);
+                        progressDialog.show();
 
-                    mDatabase.child(postID).setValue(pm).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            pd.dismiss();
-                            Toast.makeText(getApplicationContext(), "Post Successfully Posted!", Toast.LENGTH_SHORT).show();
-                            editTextPostTitle.setText("");
-                        }
-                    });
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, baos);
+                        byte[] data = baos.toByteArray();
+
+                        UploadTask uploadTask = (UploadTask) mountainsRef.putBytes(data)
+                                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                        progressDialog.dismiss();
+                                        Uri downloadlink = taskSnapshot.getDownloadUrl();
+                                        urlPhotoTemp[0] = downloadlink.toString();
+
+                                        PostModel pm = new PostModel(textPostTitle, urlPhotoTemp[0], clubName, time);
+
+                                        mDatabase.child(postID).setValue(pm).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                pd.dismiss();
+                                                Toast.makeText(getApplicationContext(), "Post Successfully Posted!", Toast.LENGTH_SHORT).show();
+                                                editTextPostTitle.setText("");
+                                            }
+                                        });
+                                        Toast.makeText(BrixxActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        progressDialog.dismiss();
+                                        Toast.makeText(BrixxActivity.this, "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                                        double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot
+                                                .getTotalByteCount());
+                                        progressDialog.setMessage("Uploaded " + (int) progress + "%");
+                                    }
+                                });
+                    } else {
+
+                        PostModel pm = new PostModel(textPostTitle, urlPhotoTemp[0], clubName, time);
+
+                        mDatabase.child(postID).setValue(pm).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                pd.dismiss();
+                                Toast.makeText(getApplicationContext(), "Post Successfully Posted!", Toast.LENGTH_SHORT).show();
+                                editTextPostTitle.setText("");
+                            }
+                        });
+                    }
                 } else {
                     pd.dismiss();
                 }
             }
         });
 
+    }
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
     }
 
     private Boolean validateCredentials() {
@@ -205,6 +283,29 @@ public class BrixxActivity extends AppCompatActivity {
         } else {
             // This is important, otherwise the result will not be passed to the fragment
             super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                    && data != null && data.getData() != null) {
+                filePath = data.getData();
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                    Toast.makeText(this, "Image selected successfully.", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+    }
+
+    protected String getSaltString() {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 18) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+
     }
 }
